8a9d1db3cc53f4a5d6cebf9601332b541dafa1d5
Navish957 <navishthakur422@gmail.com>
2024-10-01 14:23:40 +0530

    Create datatpes_basics.js
    it includes printing methods, data types, and changes

---
+++ datatpes_basics.js
@@ -0,0 +1,60 @@
+//printings in javascript
+console.log("navish");
+process.stdout.write("thakur");
+// it doesnt take linebreak
+process.stdout.write("thakur");
+
+// console.log("datatypes");
+/* 
+Strings
+Numbers
+Boolean
+Bigint = used to hold big numbers
+Undefined = may or may not have a value
+Null = must not have any value
+Object* most important
+Symbol = 
+*/
+
+// Variables - var, let & const
+let name = "navish";
+let score = 100;
+let isLoggedIn = false;
+
+//objects
+// objects using array
+let people = ["one", "two", " three"]
+// object using key value pair
+let user = {firstname: "navish" , lastname: "thakur"}
+
+let getScore = score;
+console.log(getScore);
+
+
+//DATATYPES
+
+// console.log("datatypes");
+/* 
+Strings
+Numbers
+Boolean
+Bigint = used to hold big numbers
+Undefined = may or may not have a value
+Null = must not have any value
+Object* most important
+Symbol = 
+*/
+
+// Variables - var, let & const
+let name = "navish";
+let score = 100;
+let isLoggedIn = false;
+
+//objects
+// objects using array
+let people = ["one", "two", " three"]
+// object using key value pair
+let user = {firstname: "navish" , lastname: "thakur"}
+
+let getScore = score;
+console.log(getScore);
